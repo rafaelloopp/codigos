4aa12efb672e34442634cd2d5c252571f778f9a7
rafaelloopp <45918810+rafaelloopp@users.noreply.github.com>
2021-01-08 20:18:40 -0300

    Update Nada.js

--- Nada.js
+++ Nada.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js')
 exports.run = async(client, message, args) => {
 let erro = new Discord.MessageEmbed()
-.setDescription('Você não pode usar esse comando aqui apenas em <#789308075340464178>!')
+.setDescription('Você não pode usar esse comando aqui, apenas em <#789308075340464178>!')
 .setColor('#FF0101')
 
 if(message.channel.id != 789308075340464178) return message.channel.send(erro)
+
+let embed = new Discord.MessageEmbed()
+.setDescription('Olhe sua dm!:3')
+
+message.channel.send(embed)
+
 }
