09f3fa73ece86006b9227951c0fb79ac0a33ae7f
rafaelloopp <45918810+rafaelloopp@users.noreply.github.com>
2021-01-21 09:05:25 -0300

    Update Nada.js

--- Nada.js
+++ Nada.js
@@ -2,15 +2,17 @@ const Discord = require('discord.js')
 const db = require("firebase").database()
 exports.run = async(client, message, args) => {
 let erro = new Discord.MessageEmbed()
-.setDescription('Você não pode usar esse comando aqui, apenas em <#789308075340464178>!')
+.setDescription('Você não pode usar esse comando aqui, apenas em #➕・add-bot!')
 .setColor('#FF0101')
 
 if(message.channel.id != 789308075340464178) return message.channel.send(erro)
 
+message.delete()
+
 let embed = new Discord.MessageEmbed()
 .setDescription('Olhe sua dm!:3')
 
-message.channel.send(embed)
+message.channel.send(embed).then(msg => msg.delete({timeout: 5000}))
 
 let embed2 = new Discord.MessageEmbed()
 .setDescription('qual id')
@@ -39,7 +41,14 @@ let embed5 = new Discord.MessageEmbed()
 message.author.send(embed5)
  message.author.dmChannel.createMessageCollector((x) => x.author.id == message.author.id, {max: 1}).on("collect", async (resposta4) => {
 resposta4 = resposta4.content
-.setDescription("Bot enviado para análise") 
+
+let embed6 = new Discord.MessageEmbed()
+.setDescription(`[Convide](https://discord.com/oauth2/authorize?client_id=${resposta}&scope=bot&permissions=0)
+\nID: \`${resposta}\`\nLinguagem: \`${resposta2}\`\nPrefix: \`${resposta3}\`\nDescrição: \`${resposta4}\``
+)
+client.channels.cache.get("789308076896813076").send(embed6)
+        })
+      })
+    })
   })
-})
 }
